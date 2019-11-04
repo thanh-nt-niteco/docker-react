d48037415ff069783c9466e20eb76b434e68c882
thanh-nt-niteco <thanh.nt@niteco.se>
2019-11-04 13:22:22 +0700

    remove redundant test code

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,3 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-it('renders without crashing 2', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
